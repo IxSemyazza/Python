499f4b9ce4ef7f4f964015082e35e02a4b3a11a1
IxSemyazza <michael.casallas2017@gmail.com>
2021-08-13 09:27:54 -0500

    Update Listas

--- lists.py
+++ lists.py
@@ -3,10 +3,15 @@ list = ["lunes","martes","miercoles","jueves","viernes"]
 initial = int(input("Ingrese número de la semana inicial: "))
 end = int(input("Ingrese número final del semana: "))
 print(list[initial:end])
-'''
 
-lista = [1,2,4,5,6,7,8,9,10]
+lista = [1,2,3,4,5,6,7,9,10]
 indice = int(input("Ingrese el apendice donde desea agregar el valor: "))
 valor = int(input("Ingrese el valor que desea agregar: "))
 lista.insert(indice, valor)
 print(lista)
+'''
+'''
+lista = [1,2,3]
+lista.extend([4,5,6,7,8,9])
+print(lista)
+'''
